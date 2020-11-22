15f1745e65771ca8850781d039448717486f5a35
venkatdavu <venkatadhri.davu@ggktech.com>
2020-11-22 12:40:56 +0530

    Create the master branch and made the changes

---
+++ springcloudserverconfig/src/test/java/com/davutechie/springcloud/springcloudserverconfig/SpringcloudserverconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davutechie.springcloud.springcloudserverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudserverconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
